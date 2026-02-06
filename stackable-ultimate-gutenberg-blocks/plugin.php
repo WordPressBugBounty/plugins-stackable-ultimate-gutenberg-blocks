479895bc4a9bd4f5b5f40f90755362da48d23fd4
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 03:30:49 +0000

    3.19.6

--- stackable-ultimate-gutenberg-blocks/plugin.php
+++ stackable-ultimate-gutenberg-blocks/plugin.php
@@ -7,7 +7,7 @@
  * Author: Gambit Technologies, Inc
  * Author URI: http://gambit.ph
  * Text Domain: stackable-ultimate-gutenberg-blocks
- * Version: 3.19.0
+ * Version: 3.19.6
  *
  * @package Stackable
  */
@@ -24,7 +24,7 @@ if ( !function_exists( 'stackable_multiple_plugins_check' ) ) {
     }
 
 }
-if ( defined( 'STACKABLE_FILE' ) && STACKABLE_FILE !== __FILE__ && !isset( $GLOBALS['OTHER_STACKABLE_FILE'] ) ) {
+if ( defined( 'STACKABLE_FILE' ) && STACKABLE_FILE !== __FILE__ && !isset( $GLOBALS['OTHER_STACKABLE_FILE'] ) && defined( 'STACKABLE_BUILD' ) && defined( 'STACKABLE_VERSION' ) ) {
     // Get relative file path of the other Stackable version.
     $GLOBALS['OTHER_STACKABLE_FILE'] = plugin_basename( STACKABLE_FILE );
     // Use a temporary option to store the other Stackable Plugin info needed for the admin notice. This will be deleted later.
@@ -43,7 +43,7 @@ if ( function_exists( 'sugb_fs' ) ) {
 }
 defined( 'STACKABLE_SHOW_PRO_NOTICES' ) || define( 'STACKABLE_SHOW_PRO_NOTICES', true );
 defined( 'STACKABLE_BUILD' ) || define( 'STACKABLE_BUILD', 'free' );
-defined( 'STACKABLE_VERSION' ) || define( 'STACKABLE_VERSION', '3.19.0' );
+defined( 'STACKABLE_VERSION' ) || define( 'STACKABLE_VERSION', '3.19.6' );
 defined( 'STACKABLE_FILE' ) || define( 'STACKABLE_FILE', __FILE__ );
 defined( 'STACKABLE_I18N' ) || define( 'STACKABLE_I18N', 'stackable-ultimate-gutenberg-blocks' );
 // Plugin slug.
@@ -295,6 +295,8 @@ if ( !is_admin() ) {
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/welcome/getting-started.php';
 if ( is_admin() ) {
+    require_once plugin_dir_path( __FILE__ ) . 'src/welcome/useful-plugins.php';
+    // For cross-marketing
     require_once plugin_dir_path( __FILE__ ) . 'src/welcome/index.php';
     require_once plugin_dir_path( __FILE__ ) . 'src/welcome/news.php';
     require_once plugin_dir_path( __FILE__ ) . 'src/welcome/freemius.php';
